f8cd4212110e12b14d388387aabde489ba369513
vivekmurli <vivekmurli@gmail.com>
2014-01-19 00:46:35 +0530

    added all the required files

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a + b
+        TextBox3.Text = c
+    End Sub
+End Class
