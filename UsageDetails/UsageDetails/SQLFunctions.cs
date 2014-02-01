fa74f19942ea42986f08ed330af04a965951ba1b
Anish <tycs2011@gmail.com>
2014-02-01 22:19:20 +0530

    Setting Dynamic Connnection String for SQLFunctions
    Made changes on SQLFunctions.cs and frmUsage.cs for setting Connection
    String dynamically

--- UsageDetails/UsageDetails/SQLFunctions.cs
+++ UsageDetails/UsageDetails/SQLFunctions.cs
@@ -10,40 +10,50 @@ using UsageDetails;
 namespace UsageDetails
 {
 
-    class SQLFunctions1 : DBFunctions
+    class SQLFunctions : DBFunctions
     {
+        SQLiteConnection con;
+
+
+
+        public SQLFunctions(string path)
+        {
+            con = new SQLiteConnection(path);
+        }
 
         public override List<string> cmbboxMonth()
         {
             
             List<string> month = new List<string>();
-            SQLiteConnection conn = new SQLiteConnection(@"Data Source=E:\DemoUsagePatterns.sqlite");
-            SQLiteCommand cmd = new SQLiteCommand(conn);
+            //SQLiteConnection conn = new SQLiteConnection(@"Data Source=E:\DemoUsagePatterns.sqlite");
+            SQLiteCommand cmd = new SQLiteCommand(con);
             cmd.CommandText="select DISTINCT month.month_name from month order by month.month_id";
             
-            conn.Open();
+            con.Open();
             SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
             SQLiteDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 month.Add(dr.GetString(0));
             }
+            con.Close();
             return month;
         }
 
         public override List<string> cmbboxYear()
         {
             List<string> year = new List<string>();
-            SQLiteConnection conn = new SQLiteConnection(@"Data Source=E:\DemoUsagePatterns.sqlite");
-            SQLiteCommand cmd = new SQLiteCommand(conn);
+            //SQLiteConnection conn = new SQLiteConnection(@"Data Source=E:\DemoUsagePatterns.sqlite");
+            SQLiteCommand cmd = new SQLiteCommand(con);
             cmd.CommandText = "select DISTINCT month.year from month order by month.month_id";
-            conn.Open();
+            con.Open();
             SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
             SQLiteDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 year.Add(Convert.ToString(dr.GetInt32(0)));
             }
+            con.Close();
             return year;
  
         }
@@ -53,24 +63,18 @@ namespace UsageDetails
         {
             List<string> lst = new List<string>();
 
-            string query = "SELECT water_usage.person_id, person.person_name, sum(water_usage.usage) usage, month.month_name month, month.year FROM person JOIN water_usage ON person.person_id = water_usage.person_id JOIN month ON water_usage.month_id = month.month_id GROUP BY water_usage.person_id, water_usage.month_id HAVING (month.month_name = '" + mname + "' AND month.year= " +year + " AND sum(water_usage.usage) > "+usage +")";
+            string query = "SELECT water_usage.person_id, person.person_name, sum(water_usage.usage) usage, month.month_name month, month.year FROM person JOIN water_usage ON person.person_id = water_usage.person_id JOIN month ON water_usage.month_id = month.month_id GROUP BY water_usage.person_id, water_usage.month_id HAVING (month.month_name = '" + mname + "' AND month.year="+ year +" AND sum(water_usage.usage) >"+ usage+")";
 
-            using (SQLiteConnection con = new SQLiteConnection(@"Data Source=E:\db\DemoUsagePattern.sqlite"))
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+            con.Open();
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
             {
-                SQLiteCommand cmd = new SQLiteCommand(query, con);
-                con.Open();
-                SQLiteDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-
-                    lst.Add(reader.GetString(2));
-                    
-                }
-                
-                return lst;
-               }
-
+                lst.Add(reader.GetString(1));
+            }
+            con.Close();
+            return lst;
         }
         
     }
